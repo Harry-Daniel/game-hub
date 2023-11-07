fa695d51a32a10424242b43e98ae3709e2fff2df
Harry-Daniel <hayiborkomla@gmail.com>
2023-11-07 13:28:57 +0000

    Refactor GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,9 +17,11 @@ const GameGrid = ({gameQuery}:Props) => {
   const skeletons =[1,2,3,4,5,6,7,8,9,10];
 
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-        {error && <Text>{error}</Text>}
+    
+        
 
         <SimpleGrid columns= {{sm:1 , md:2, lg:3, xl:4, "2xl":5}} padding='10px' spacing= {6}>
             
@@ -31,7 +33,7 @@ const GameGrid = ({gameQuery}:Props) => {
             ))}
         </SimpleGrid>
 
-    </>
+    
   )
 }
 
