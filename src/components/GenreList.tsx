6ce542265f3190e795b52d56124523b00aa47de3
Harry-Daniel <hayiborkomla@gmail.com>
2023-12-28 16:37:27 +0000

    Refactor: Manage State with Zustand

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,16 @@
-import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
-import useGenres, { Genre } from '../hooks/useGenres'
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from '@chakra-ui/react';
+import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
-
-interface Props{
-  onSelectGenre: (genre: Genre)=> void;
-  selectedGenreId?: number;
-}
+import useGameQueryStore from '../store';
 
 
-const GenreList = ({onSelectGenre,selectedGenreId}:Props) => {
+
+
+const GenreList = () => {
     const {data, isLoading, error}=useGenres();
-    
+    const selectedGenreId=useGameQueryStore(s=> s.gameQuery.genreId);
+    const setSelectedGenreId=useGameQueryStore(s=> s.setGenreId)
+
     if (error) return null;
     if (isLoading) return <Spinner/>
     return (
@@ -20,7 +20,7 @@ const GenreList = ({onSelectGenre,selectedGenreId}:Props) => {
        {data?.results.map(genre=> <ListItem key={genre.id} paddingY='5px'>
         <HStack>
           <Image objectFit='cover' boxSize='32px' borderRadius='8px' src={getCroppedImageUrl(genre.image_background)}/>
-          <Button textAlign='left' whiteSpace='normal' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link' >
+          <Button textAlign='left' whiteSpace='normal' fontWeight={genre.id === selectedGenreId ? 'bold' : 'normal'} onClick={()=> setSelectedGenreId(genre.id)} fontSize='lg' variant='link' >
             {genre.name}
           </Button>
         
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,13 +1,10 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import { BsChevronDown } from 'react-icons/bs'
-
-interface Props{
-    onSelectSortOrder: (sortOrder :string)=>void;
-    sortOrder: string;
-}
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import useGameQueryStore from '../store';
 
 
-const SortSelector = ({onSelectSortOrder, sortOrder}:Props) => {
+
+const SortSelector = () => {
     const sortOrders = [
         {value: '', label: 'Relevance'},
         {value: '-added', label: 'Date Added'},
@@ -16,6 +13,9 @@ const SortSelector = ({onSelectSortOrder, sortOrder}:Props) => {
         {value: '-metacritic', label: 'Popularity'},
         {value: '-rating', label: 'Average Rating'},
     ];
+
+    const setSortOrder = useGameQueryStore(s=>s.setSortOrder);
+    const sortOrder = useGameQueryStore(s=>s.gameQuery.sortOrder);
 
     const currentSortOrder= sortOrders.find(order=> order.value === sortOrder);
 
@@ -28,7 +28,7 @@ const SortSelector = ({onSelectSortOrder, sortOrder}:Props) => {
 
         <MenuList>
            {sortOrders.map((order)=> 
-           <MenuItem onClick={()=> onSelectSortOrder(order.value)} key={order.value} value={order.value}>
+           <MenuItem onClick={()=> setSortOrder(order.value)} key={order.value} value={order.value}>
                 {order.label}
            </MenuItem>)}
         </MenuList>
